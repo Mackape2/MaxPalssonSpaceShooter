7ffe1e166b0b3b409082cff6a912e4be98737c46
Mackape2 <Mackape841@gmail.com>
2023-11-01 11:45:01 +0100

    Added ISystem that handles update

--- SpaceMP/Assets/Scripts/SpaceAspect.cs
+++ SpaceMP/Assets/Scripts/SpaceAspect.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
 
 namespace AsteroidsNamespace
@@ -7,6 +8,35 @@ namespace AsteroidsNamespace
     {
         public readonly Entity Entity;
 
-        //private readonly TransformAspect _transformAspect;
+        //Mentalnote: RefRO = reference Read only
+        //            RefRW = reference Read Write
+        private readonly RefRO<Asteroids> _asteroids;
+        private readonly RefRW<SpaceRandomGen> _spaceRandomGen;
+        private readonly RefRW<LocalTransform> _transform;
+        private LocalTransform Transform => _transform.ValueRW;
+
+        public int NumberAsteroidsToSpawn => _asteroids.ValueRO.NumberOfSpawnpoints;
+        public Entity AsteroidPrefab => _asteroids.ValueRO.SpawnpointPrefab;
+
+
+        private float3 GetRandomPosition()
+        {
+            float3 randomPosition;
+            do
+            {
+                randomPosition = _spaceRandomGen.ValueRW.Value.NextFloat3(MinCorner,MaxCorner);
+                
+            } while (math.distancesq(Transform.Position, randomPosition) <= SpawnFreeArea);
+
+
+            return randomPosition;
+            
+        }
+        private float3 MinCorner => Transform.Position - (_asteroids.ValueRO.FieldDimensions.x * (float)0.5);
+        private float3 MaxCorner => Transform.Position + (_asteroids.ValueRO.FieldDimensions.y * (float)0.5);
+        public float3 Position => Transform.Position;
+
+        private const float SpawnFreeArea = 100;
+
     }
 }
---
+++ SpaceMP/Assets/Scripts/Spawning system.cs
@@ -0,0 +1,40 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.VisualScripting;
+
+namespace AsteroidsNamespace
+{
+    [BurstCompile]
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    public partial struct SpawningSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<Asteroids>();
+        }
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+            
+        }
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            state.Enabled = false;
+            var spaceEntitiy = SystemAPI.GetSingletonEntity<Asteroids>();
+            var space = SystemAPI.GetAspect<SpaceAspect>(spaceEntitiy);
+
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+            for (int i = 0; i < space.NumberAsteroidsToSpawn; i++)
+            {
+                ecb.Instantiate(space.AsteroidPrefab);
+            }
+            
+            ecb.Playback(state.EntityManager);
+        }
+
+    }
+}
